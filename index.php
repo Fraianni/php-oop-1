b7b83a88bf6ddd9dced123ae4740f79bbae1b414
Fraianni <francescoianni1995@gmail.com>
2022-10-25 15:03:10 +0200

    movie

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+class Movie
+{
+    public $nome;
+    public $genre;
+    public $vote = 0;
+
+
+    function __construct($_nome, $_genre, $_vote = 0)
+    {
+        $this->nome = $_nome;
+        $this->genre = $_genre;
+        $this->setVote($_vote);
+    }
+
+    private function setVote($_vote)
+    {
+        if ($_vote <= 10)
+            $this->vote = $_vote;
+        else {
+            while ($_vote > 10) {
+                $_vote = $_vote / 10;
+            }
+            $this->vote = $_vote;
+        }
+    }
+
+
+
+
+    public function __set($name, $value)
+    {
+        throw new Exception("Cannot add new property \$$name to instance of " . __CLASS__);
+    }
+}
+
+$rocky = new Movie("rocky", "azione", 110);
+echo ($rocky->vote . '<br>');
